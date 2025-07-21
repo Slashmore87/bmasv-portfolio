82ea156c5c5c3b7c29a9613fd509f0ed33668190
Slashmore87 <55769031+Slashmore87@users.noreply.github.com>
2025-07-21 16:04:50 -0400

    Add files via upload

---
+++ automate.py
@@ -0,0 +1,72 @@
+import os
+import re
+import yaml
+
+CARPETA_PROYECTOS = "proyectos"
+ARCHIVO_HTML = "index.html"
+
+def cargar_proyectos():
+    proyectos = []
+    for archivo in os.listdir(CARPETA_PROYECTOS):
+        if archivo.endswith(".md"):
+            with open(os.path.join(CARPETA_PROYECTOS, archivo), encoding="utf-8") as f:
+                contenido = f.read()
+                frontmatter = re.match(r"---(.*?)---", contenido, re.DOTALL)
+                if not frontmatter:
+                    continue
+                data = yaml.safe_load(frontmatter.group(1))
+                proyectos.append(data)
+    return sorted(proyectos, key=lambda x: (-int(str(x.get("year", 0))[:4]), x.get("title", "")))
+
+def generar_html(proyecto, index):
+    title = proyecto.get("title", "")
+    year = proyecto.get("year", "")
+    description = proyecto.get("description", "")
+    thumbnail = proyecto.get("thumbnail", "")
+    imagenes = proyecto.get("imagen_full", [])
+    filtro = proyecto.get("filtro", "arq")
+    if isinstance(imagenes, str):
+        imagenes = [imagenes]
+    gallery_id = f"gallery{100+index}"
+
+    # Primer enlace, muestra overlay y título
+    bloque = f'''                <li class="item-thumbs span3 {filtro}">
+                  <!-- Fancybox - Gallery Enabled - Title - Full Image --> <a class="hover-wrap fancybox"
+                    data-fancybox-group="{gallery_id}" title="{title}"
+                    href="{imagenes[0] if imagenes else ''}">
+                    <span class="overlay-img"></span> <span class="overlay-img-thumb">{title}<br>
+                      {year}</span> </a>\n'''
+
+    # Enlaces adicionales (si hay más de una imagen)
+    for img in imagenes[1:]:
+        bloque += f'                  <a class="hover-wrap fancybox" data-fancybox-group="{gallery_id}" title="{title}" href="{img}"></a>\n'
+
+    bloque += f'                  <!-- Thumb Image and Description --> <img src="{thumbnail}">\n                </li>\n'
+    return bloque
+
+def reemplazar_seccion(html, inicio, fin, nuevo_bloque):
+    return re.sub(
+        rf"({inicio})(.|\s)*?({fin})",
+        f"{inicio}\n{nuevo_bloque}                {fin}",
+        html,
+        flags=re.DOTALL
+    )
+
+if __name__ == "__main__":
+    proyectos = cargar_proyectos()
+    html_proyectos = "".join([generar_html(p, i) for i, p in enumerate(proyectos)])
+
+    with open(ARCHIVO_HTML, encoding="utf-8") as f:
+        html = f.read()
+
+    html_nuevo = reemplazar_seccion(
+        html,
+        "<!-- INICIO_PROYECTOS -->",
+        "<!-- End Item Project -->",
+        html_proyectos
+    )
+
+    with open(ARCHIVO_HTML, "w", encoding="utf-8") as f:
+        f.write(html_nuevo)
+
+    print("¡Proyectos actualizados en el index.html!")
